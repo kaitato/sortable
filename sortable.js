031adf39b0d0973105bd741826350fad33208a71
sow <sur_sowj@yahoo.co.uk>
2022-06-18 16:54:34 +0100

    displays all data including images in the table

--- sortable.js
+++ sortable.js
@@ -23,22 +23,27 @@ function displayHeroData(loadData) {
     //     IconDiv.appendChild(IconImg);
     //     document.body.style.backgroundImage = "url('" + cocktail.strDrinkThumb + "')";
     // }
-    let col = []
-    for (let i = 0; i < loadData.length; i++) {
-        for (let key in loadData[i]) {
-            if (col.indexOf(key) === -1) {
-                col.push(key);
-            }
-        }
-    }
-    let table = document.createElement("table");
+    let col = ['images','name','biography','powerstats','appearance'];
+    let bio = ['fullName', 'placeOfBirth', 'alignment']
+    let app = ['race', 'gender', 'height', 'weight']
+    let pow = ['intelligence', 'strength', 'speed', 'durability', 'power', 'combat']
+    // let col = []
+    // for (let i = 0; i < loadData.length; i++) {
+    //     for (let key in loadData[i]) {
+    //         if (col.indexOf(key) === -1) {
+    //             console.log(key)
+    //             col.push(key);
+    //         }
+    //     }
+    // }
+    let table = document.getElementById("table");
     let tr = table.insertRow(-1);                   // table row.
 
-    for (let i = 0; i < col.length; i++) {
-        let th = document.createElement("th");      // table header.
-        th.innerHTML = col[i];
-        tr.appendChild(th);
-    }
+    // for (let i = 0; i < col.length; i++) {
+    //     let th = document.createElement("th");      // table header.
+    //     th.innerHTML = col[i];
+    //     tr.appendChild(th);
+    // }
 
     // add json data to the table as rows.
     for (let i = 0; i < loadData.length; i++) {
@@ -46,22 +51,41 @@ function displayHeroData(loadData) {
         tr = table.insertRow(-1);
 
         for (let j = 0; j < col.length; j++) {
-            if (typeof loadData[i][col[j]] === "object") {
-                for (let k = 0; k < col[j].length; k++) {
+            if (col[j]=== 'biography') {
+                for (let k = 0; k < bio.length; k++) {
                     let tabCell = tr.insertCell(-1);
-                    tabCell.innerHTML = loadData[i][col[j][k]];
+                    tabCell.innerHTML = loadData[i][col[j]][bio[k]];
                 }
+            } else if (col[j] === 'appearance') {
+                for (let k = 0; k < app.length; k++) {
+                    let tabCell = tr.insertCell(-1);
+                    tabCell.innerHTML = loadData[i][col[j]][app[k]];
+                }
+            } else if (col[j] === 'powerstats'){
+                for (let k = 0; k < pow.length; k++) {
+                    let tabCell = tr.insertCell(-1);
+                    tabCell.innerHTML = loadData[i][col[j]][pow[k]];
+                }
+            }else if (col[j] ==='images'){
+                const IconImg = document.createElement("img");
+                IconImg.src = loadData[i].images.xs;
+                let tabCell = tr.insertCell(-1);
+                    tabCell.appendChild(IconImg);
+            
             } else {
                 let tabCell = tr.insertCell(-1);
+                
                 tabCell.innerHTML = loadData[i][col[j]];
             }
+           
+            
         }
     }
-
     // Now, add the newly created table with json data, to a container.
     let divShowData = document.getElementById('showData');
     divShowData.innerHTML = "";
     divShowData.appendChild(table);
     
     document.getElementById('msg').innerHTML = '<br />You can later <a href="https://www.encodedna.com/javascript/dynamically-add-remove-rows-to-html-table-using-javascript-and-save-data.htm" target="_blank" style="color:#1464f4;text-decoration:none;">get all the data from table and save it in a database.</a>';
+
 }
